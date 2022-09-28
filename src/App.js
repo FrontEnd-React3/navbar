817f97a137686084f1be96d4d3b747a2bb25b8d4
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2022-09-28 09:52:26 +0200

    r

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <ul className= "navbar">
+          <li>01</li>
+          <li>02</li>
+          <li>03</li>
+          <li>04</li>
+          <li>05</li>
+          <li>06</li>
+          <li>07</li>
+          <li>08</li>
+          <li>09</li>
+          <li>10</li>
+          <li>11</li>
+          <li>12</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
